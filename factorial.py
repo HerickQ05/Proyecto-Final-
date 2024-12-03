93c786454b2cbee57c459b999c0ed0ef51e82cec
HerickQ05 <quispeherick42@gmail.com>
2024-12-03 14:16:01 -0500

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n - 1)
-num = int(input("Ingresa un número entero positivo: "))
-if num < 0:
-    print("El número debe ser positivo.")
-else:
-    print(f"El factorial de {num} es: {factorial(num)}")
